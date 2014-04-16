ce85b3e218284bda8f41255b17fc80b0eb0d6d79
Joann Kim <jnk006@ucsd.edu>
2014-04-16 12:15:24 -0700

    fb authentication

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var conf = {
   , client_secret:  '51956c86a09a2a79c4b4b3363812551a'
   , scope:          'email, user_about_me, user_birthday, user_location, publish_stream, read_stream'
   //, redirect_uri:   'http://localhost:3000/auth/facebook'
-  , redirect_uri:   'http://assignment1-cogs121.herokuapp.com'
+  , redirect_uri:   'http://assignment1-cogs121.herokuapp.com/auth/facebook'
 };
 
 app.get('/auth/facebook', function(req, res) {
